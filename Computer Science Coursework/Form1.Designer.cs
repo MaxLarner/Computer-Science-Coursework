9415aa4b4d648236b11575bf5b6fcc5dc360e081
max larner <06mlarner@brightoncollege.net>
2020-09-27 08:42:55 -0700

    added global variables

--- Computer Science Coursework/Form1.Designer.cs
+++ Computer Science Coursework/Form1.Designer.cs
@@ -31,6 +31,14 @@
             this.btn_WallSubmit = new System.Windows.Forms.Button();
             this.txt_WallWidth = new System.Windows.Forms.TextBox();
             this.txt_WallHeight = new System.Windows.Forms.TextBox();
+            this.pnl_Holds = new System.Windows.Forms.Panel();
+            this.pctBox_GreenHold = new System.Windows.Forms.PictureBox();
+            this.pctBox_BlueHold = new System.Windows.Forms.PictureBox();
+            this.pctBox_RedHold = new System.Windows.Forms.PictureBox();
+            this.pnl_Holds.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox_GreenHold)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox_BlueHold)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox_RedHold)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_WallSubmit
@@ -57,17 +65,62 @@
             this.txt_WallHeight.Size = new System.Drawing.Size(89, 23);
             this.txt_WallHeight.TabIndex = 1;
             // 
+            // pnl_Holds
+            // 
+            this.pnl_Holds.Controls.Add(this.pctBox_GreenHold);
+            this.pnl_Holds.Controls.Add(this.pctBox_BlueHold);
+            this.pnl_Holds.Controls.Add(this.pctBox_RedHold);
+            this.pnl_Holds.Location = new System.Drawing.Point(280, 560);
+            this.pnl_Holds.Name = "pnl_Holds";
+            this.pnl_Holds.Size = new System.Drawing.Size(680, 160);
+            this.pnl_Holds.TabIndex = 2;
+            // 
+            // pctBox_GreenHold
+            // 
+            this.pctBox_GreenHold.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.pctBox_GreenHold.Location = new System.Drawing.Point(477, 26);
+            this.pctBox_GreenHold.Name = "pctBox_GreenHold";
+            this.pctBox_GreenHold.Size = new System.Drawing.Size(156, 119);
+            this.pctBox_GreenHold.TabIndex = 2;
+            this.pctBox_GreenHold.TabStop = false;
+            this.pctBox_GreenHold.Click += new System.EventHandler(this.pctBox_GreenHold_Click);
+            // 
+            // pctBox_BlueHold
+            // 
+            this.pctBox_BlueHold.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pctBox_BlueHold.Location = new System.Drawing.Point(260, 26);
+            this.pctBox_BlueHold.Name = "pctBox_BlueHold";
+            this.pctBox_BlueHold.Size = new System.Drawing.Size(158, 119);
+            this.pctBox_BlueHold.TabIndex = 1;
+            this.pctBox_BlueHold.TabStop = false;
+            this.pctBox_BlueHold.Click += new System.EventHandler(this.pctBox_BlueHold_Click);
+            // 
+            // pctBox_RedHold
+            // 
+            this.pctBox_RedHold.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.pctBox_RedHold.Location = new System.Drawing.Point(63, 26);
+            this.pctBox_RedHold.Name = "pctBox_RedHold";
+            this.pctBox_RedHold.Size = new System.Drawing.Size(141, 119);
+            this.pctBox_RedHold.TabIndex = 0;
+            this.pctBox_RedHold.TabStop = false;
+            this.pctBox_RedHold.Click += new System.EventHandler(this.pctBox_RedHold_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(769, 450);
+            this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.Controls.Add(this.pnl_Holds);
             this.Controls.Add(this.txt_WallHeight);
             this.Controls.Add(this.txt_WallWidth);
             this.Controls.Add(this.btn_WallSubmit);
             this.Name = "Form1";
             this.Text = "Wall Build";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnl_Holds.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox_GreenHold)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox_BlueHold)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox_RedHold)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,6 +131,10 @@
         private System.Windows.Forms.Button btn_WallSubmit;
         private System.Windows.Forms.TextBox txt_WallWidth;
         private System.Windows.Forms.TextBox txt_WallHeight;
+        private System.Windows.Forms.Panel pnl_Holds;
+        private System.Windows.Forms.PictureBox pctBox_BlueHold;
+        private System.Windows.Forms.PictureBox pctBox_RedHold;
+        private System.Windows.Forms.PictureBox pctBox_GreenHold;
     }
 }
 
--- Computer Science Coursework/Form1.cs
+++ Computer Science Coursework/Form1.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,12 @@ namespace Computer_Science_Coursework
 {
     public partial class Form1 : Form
     {
+        int HoldCount = 0;
+        bool Red = false;
+        bool Green = false;
+        bool Blue = false;
+        Panel WallPanel = new Panel();
+
         public Form1()
         {
             InitializeComponent();
@@ -29,18 +36,131 @@ namespace Computer_Science_Coursework
             int WallWidth = int.Parse(txt_WallWidth.Text);
             int WallHeight = int.Parse(txt_WallHeight.Text);
 
-            MessageBox.Show(txt_WallWidth.Text +", " + txt_WallHeight.Text, "Wall Size");
+            MessageBox.Show(txt_WallWidth.Text + ", " + txt_WallHeight.Text, "Wall Size");
 
             CreateWall(WallWidth, WallHeight);
         }
 
         private void CreateWall(int X, int Y)
+        { 
+            WallPanel.Location = new System.Drawing.Point(300, 50);
+            WallPanel.Name = "pnl_ClimbingWall";
+            WallPanel.Size = new System.Drawing.Size(X, Y);
+            WallPanel.BackColor = Color.LightGreen;
+            WallPanel.MouseClick+=WallPanel_Click;
+            Controls.Add(WallPanel);
+        }
+
+        private void AddHold(int x, int y, PictureBox pctBox_Hold)
         {
-            Canvas WallCanvas = new Canvas();
+            
+            pctBox_Hold.Name = "pnl_Hold" + Convert.ToString(HoldCount);
+            pctBox_Hold.Location = new Point(x, y);
+            HoldCount++;
+            WallPanel.Controls.Add(pctBox_Hold);
+        }
 
-            WallCanvas.Background = new SolidColorBrush(Color.LightCyan);
+        private void WallPanel_Click(object sender, MouseEventArgs e)
+        {
+            int xMouseClick = e.X;
+            int yMouseClick = e.Y;
 
+            MessageBox.Show(Convert.ToString(xMouseClick), Convert.ToString(yMouseClick));
 
+            PictureBox pctBox_Temp = new PictureBox();
+
+            if (Blue == true)
+            {
+                pctBox_Temp = pctBox_BlueHold;
+            }
+            else if (Green == true)
+            {
+                pctBox_Temp = pctBox_BlueHold;
+            }
+            else if (Red == true)
+            {
+                pctBox_Temp = pctBox_GreenHold;
+            }
+
+            AddHold(xMouseClick, yMouseClick, pctBox_Temp);
+        }
+        public void pctBox_GreenHold_Click(object sender, EventArgs e)
+        {
+            Green = true;
+            Red = false;
+            Blue = false;
+            ///
+            PictureBox pctBox_GreenHold = new PictureBox();
+
+            System.Drawing.Drawing2D.GraphicsPath pictureBoxPath = new System.Drawing.Drawing2D.GraphicsPath();
+
+            System.Drawing.Rectangle newRectangle = pctBox_GreenHold.ClientRectangle;
+
+            Point point1 = new Point(50, 0);
+            Point point2 = new Point(100, 25);
+            Point point3 = new Point(50, 50);
+            Point point4 = new Point(0, 25);
+            Point[] rhombus = { point1, point2, point3, point4 };
+
+            pictureBoxPath.AddPolygon(rhombus);
+
+            pctBox_GreenHold.Region = new System.Drawing.Region(pictureBoxPath);
+            pctBox_GreenHold.BackColor = Color.AliceBlue;
+
+            MessageBox.Show("Green Clicked");
+        }
+
+        public void pctBox_BlueHold_Click(object sender, EventArgs e)
+        {
+            Green = false;
+            Blue = true;
+            Red = false;
+            // Creating a rhombus picture box
+
+            PictureBox pctBox_BlueHold = new PictureBox();
+
+            System.Drawing.Drawing2D.GraphicsPath pictureBoxPath = new System.Drawing.Drawing2D.GraphicsPath();
+            
+            System.Drawing.Rectangle newRectangle = pctBox_BlueHold.ClientRectangle;
+            
+            Point point1 = new Point(50, 0); 
+            Point point2 = new Point(100, 25); 
+            Point point3 = new Point(50, 50); 
+            Point point4 = new Point(0, 25); 
+            Point[] rhombus = { point1, point2, point3, point4 };
+            
+            pictureBoxPath.AddPolygon(rhombus);
+
+            pctBox_BlueHold.Region = new System.Drawing.Region(pictureBoxPath);
+            pctBox_BlueHold.BackColor = Color.AliceBlue;
+            
+            MessageBox.Show("Blue Clicked");
+        }
+
+        public void pctBox_RedHold_Click(object sender, EventArgs e)
+        {
+            Green = false;
+            Red = true;
+            Blue = false;
+            ///
+            PictureBox pctBox_RedHold = new PictureBox();
+
+            System.Drawing.Drawing2D.GraphicsPath pictureBoxPath = new System.Drawing.Drawing2D.GraphicsPath();
+
+            System.Drawing.Rectangle newRectangle = pctBox_RedHold.ClientRectangle;
+
+            Point point1 = new Point(50, 0);
+            Point point2 = new Point(100, 25);
+            Point point3 = new Point(50, 50);
+            Point point4 = new Point(0, 25);
+            Point[] rhombus = { point1, point2, point3, point4 };
+
+            pictureBoxPath.AddPolygon(rhombus);
+
+            pctBox_RedHold.Region = new System.Drawing.Region(pictureBoxPath);
+            pctBox_RedHold.BackColor = Color.Red;
+
+            MessageBox.Show("Red Clicked");
         }
     }
 }
