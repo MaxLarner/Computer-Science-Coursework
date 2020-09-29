7eba4ed0bee8d2a129f85fbecb689744d51b4b9f
max larner <06mlarner@brightoncollege.net>
2020-09-29 03:34:16 -0700

    Can add shaped polygons to the wall
    when a button is selected, it dissapears?
    Can only add one polygon of each colour?

--- Computer Science Coursework/Form1.cs
+++ Computer Science Coursework/Form1.cs
@@ -10,6 +10,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
 
 namespace Computer_Science_Coursework
 {
@@ -54,7 +56,7 @@ namespace Computer_Science_Coursework
         private void AddHold(int x, int y, PictureBox pctBox_Hold)
         {
             
-            pctBox_Hold.Name = "pnl_Hold" + Convert.ToString(HoldCount);
+            pctBox_Hold.Name = "pctBox_Hold" + Convert.ToString(HoldCount);
             pctBox_Hold.Location = new Point(x, y);
             HoldCount++;
             WallPanel.Controls.Add(pctBox_Hold);
@@ -72,93 +74,126 @@ namespace Computer_Science_Coursework
             if (Blue == true)
             {
                 pctBox_Temp = pctBox_BlueHold;
+                pctBox_Temp.Paint += new PaintEventHandler(pctBox_BlueHold_Paint);
             }
             else if (Green == true)
             {
-                pctBox_Temp = pctBox_BlueHold;
+                pctBox_Temp = pctBox_GreenHold;
+                pctBox_Temp.Paint += new PaintEventHandler(pctBox_GreenHold_Paint);
             }
             else if (Red == true)
             {
-                pctBox_Temp = pctBox_GreenHold;
+                pctBox_Temp = pctBox_RedHold;
+                pctBox_Temp.Paint += new PaintEventHandler(pctBox_RedHold_Paint);
             }
 
             AddHold(xMouseClick, yMouseClick, pctBox_Temp);
         }
+
         public void pctBox_GreenHold_Click(object sender, EventArgs e)
         {
+            // Creates a picturebox called pctBox_GreenHold
+            PictureBox pctBox_GreenHold = new PictureBox();
             Green = true;
             Red = false;
             Blue = false;
-            ///
-            PictureBox pctBox_GreenHold = new PictureBox();
+        }
 
-            System.Drawing.Drawing2D.GraphicsPath pictureBoxPath = new System.Drawing.Drawing2D.GraphicsPath();
+        private void pctBox_GreenHold_Paint(object sender, PaintEventArgs e)
+        {
+            pctBox_GreenHold.BackColor = Color.Green;
 
-            System.Drawing.Rectangle newRectangle = pctBox_GreenHold.ClientRectangle;
+            GraphicsPath pctBox_Path = new GraphicsPath();
 
-            Point point1 = new Point(50, 0);
-            Point point2 = new Point(100, 25);
-            Point point3 = new Point(50, 50);
-            Point point4 = new Point(0, 25);
-            Point[] rhombus = { point1, point2, point3, point4 };
+            // The points of the rhombus in n array, creates the polygon
+            Point[] Rhombus =
+                {
+                    new Point(23, 20),
+                    new Point(40, 10),
+                    new Point(57, 20),
+                    new Point(50, 40),
+                    new Point(30, 40)
+                    };
 
-            pictureBoxPath.AddPolygon(rhombus);
+            // Adds the rhombus to the graphics path
+           
+            pctBox_Path.AddPolygon(Rhombus);
 
-            pctBox_GreenHold.Region = new System.Drawing.Region(pictureBoxPath);
-            pctBox_GreenHold.BackColor = Color.AliceBlue;
+            //the region of the picturebox is now set to the shape of the graphics path
+            pctBox_GreenHold.Region = new Region(pctBox_Path);
 
             MessageBox.Show("Green Clicked");
         }
 
         public void pctBox_BlueHold_Click(object sender, EventArgs e)
         {
+            PictureBox pctBox_BlueHold = new PictureBox();
             Green = false;
             Blue = true;
             Red = false;
             // Creating a rhombus picture box
+            
+            MessageBox.Show("Blue Clicked");
+        }
 
-            PictureBox pctBox_BlueHold = new PictureBox();
+        public void pctBox_BlueHold_Paint(object sender, PaintEventArgs e)
+        {
+            // Creates a picturebox called pctBox_GreenHold
+            pctBox_BlueHold.BackColor = Color.Blue;
 
-            System.Drawing.Drawing2D.GraphicsPath pictureBoxPath = new System.Drawing.Drawing2D.GraphicsPath();
-            
-            System.Drawing.Rectangle newRectangle = pctBox_BlueHold.ClientRectangle;
-            
-            Point point1 = new Point(50, 0); 
-            Point point2 = new Point(100, 25); 
-            Point point3 = new Point(50, 50); 
-            Point point4 = new Point(0, 25); 
-            Point[] rhombus = { point1, point2, point3, point4 };
-            
-            pictureBoxPath.AddPolygon(rhombus);
+            GraphicsPath pctBox_Path = new GraphicsPath();
 
-            pctBox_BlueHold.Region = new System.Drawing.Region(pictureBoxPath);
-            pctBox_BlueHold.BackColor = Color.AliceBlue;
-            
+            // The points of the rhombus in n array, creates the polygon
+            Point[] Rhombus =
+                {
+                    new Point(23, 20),
+                    new Point(40, 10),
+                    new Point(57, 20),
+                    new Point(50, 40),
+                    new Point(30, 40)
+                    };
+
+            // Adds the rhombus to the graphics path
+
+            pctBox_Path.AddPolygon(Rhombus);
+
+            //the region of the picturebox is now set to the shape of the graphics path
+            pctBox_BlueHold.Region = new Region(pctBox_Path);
+
             MessageBox.Show("Blue Clicked");
         }
 
         public void pctBox_RedHold_Click(object sender, EventArgs e)
         {
+            PictureBox pctBox_RedHold = new PictureBox();
             Green = false;
             Red = true;
             Blue = false;
-            ///
-            PictureBox pctBox_RedHold = new PictureBox();
+        }
 
-            System.Drawing.Drawing2D.GraphicsPath pictureBoxPath = new System.Drawing.Drawing2D.GraphicsPath();
-
-            System.Drawing.Rectangle newRectangle = pctBox_RedHold.ClientRectangle;
-
-            Point point1 = new Point(50, 0);
-            Point point2 = new Point(100, 25);
-            Point point3 = new Point(50, 50);
-            Point point4 = new Point(0, 25);
-            Point[] rhombus = { point1, point2, point3, point4 };
-
-            pictureBoxPath.AddPolygon(rhombus);
-
-            pctBox_RedHold.Region = new System.Drawing.Region(pictureBoxPath);
+        public void pctBox_RedHold_Paint(object sender, PaintEventArgs e)
+        {
+            // Creates a picturebox called pctBox_GreenHold
             pctBox_RedHold.BackColor = Color.Red;
+
+            GraphicsPath pctBox_Path = new GraphicsPath();
+
+            // The points of the rhombus in n array, creates the polygon
+            Point[] Rhombus =
+                {
+                    new Point(23, 20),
+                    new Point(40, 10),
+                    new Point(57, 20),
+                    new Point(50, 40),
+                    new Point(30, 40)
+                    };
+
+            // Adds the rhombus to the graphics path
+
+            pctBox_Path.AddPolygon(Rhombus);
+
+            //the region of the picturebox is now set to the shape of the graphics path
+            pctBox_RedHold.Region = new Region(pctBox_Path);
 
             MessageBox.Show("Red Clicked");
         }
